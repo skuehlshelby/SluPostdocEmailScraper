ec9176047ea44a70ddb9f95e9ebc56d66e70a51f
leforero <leslieeforero@gmail.com>
2022-12-04 11:25:31 +0100

    Add Campus class

---
+++ ScrapeSluPostdocEmails/Campus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SluEmailScraper
+{
+    public class Campus
+    {
+        public string Name { get; set; }
+        public string[] Departments { get; set; }
+    }
+}
